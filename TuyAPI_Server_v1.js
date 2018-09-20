90b398e81c43719eeae1789df7939a006f3d078a
Ivan Kunz <ivan.kunz@openet.us>
2018-09-19 22:14:23 -0400

    changes

--- TuyAPI_Server_v1.js
+++ TuyAPI_Server_v1.js
@@ -23,28 +23,28 @@ console.log("TuyAPI Hub Console Log")
 logResponse("\n\r" + new Date() + "\rTuyAPI Hub Error Log")
 
 //---- Command interface to Smart Things ---------------------------
-function onRequest(request, response){
-	let command = 	request.headers["command"]
-	let deviceIP = 	request.headers["tuyapi-ip"]
-	
+function onRequest(request, response) {
+	let command = request.headers["command"]
+	let deviceIP = request.headers["tuyapi-ip"]
+
 	let cmdRcvd = "\n\r" + new Date() + "\r\nIP: " + deviceIP + " sent command " + command
 	console.log(" ")
 	console.log(cmdRcvd)
-		
-	switch(command) {
+
+	switch (command) {
 		//---- (BridgeDH - Poll for Server APP ------------------
 		case "pollServer":
 			response.setHeader("cmd-response", "ok")
 			response.end()
 			let respMsg = "Server Poll response sent to SmartThings"
 			console.log(respMsg)
-		break
+			break
 
 		//---- TP-Link Device Command ---------------------------
 		case "deviceCommand":
 			processDeviceCommand(request, response)
 			break
-	
+
 		default:
 			response.setHeader("cmd-response", "InvalidHubCmd")
 			response.end()
@@ -57,22 +57,87 @@ function onRequest(request, response){
 
 //---- Send deviceCommand and send response to SmartThings ---------
 function processDeviceCommand(request, response) {
-	
+
 	let deviceIP = request.headers["tuyapi-ip"]
 	let deviceID = request.headers["tuyapi-devid"]
 	let localKey = request.headers["tuyapi-localkey"]
-	let command =  request.headers["tuyapi-command"]
-        let dps = request.headers["dps"]
-        let action = request.headers["action"]
+	let command = request.headers["tuyapi-command"]
+	let dps = request.headers["dps"]
+	let action = request.headers["action"]
 	let respMsg = "deviceCommand sending to IP: " + deviceIP + " Command: " + command
 	console.log(respMsg)
 
 	let tuya = new TuyaDevice({
-	  id: deviceID,
-	  ip: deviceIP,
-          dps: '4',
-	  key: localKey});
-	
+		id: deviceID,
+		ip: deviceIP,
+		dps: '4',
+		key: localKey
+	});
+	switch (command) {
+		case "off":
+			//tuya.resolveIds().then(() => {  
+			tuya.get({ 'dps': dps }).then(status => {
+				console.log('Status: ' + status);
+
+				tuya.set({ set: false, 'dps': dps }).then(result => {
+					console.log('Result of setting status to ' + false + ': ' + result);
+					response.setHeader("cmd-response", !status);
+					response.setHeader("onoff", "off");
+					console.log("Status (" + status + ") sent to SmartThings.");
+					response.end();
+
+
+					return;
+
+				});
+				//});
+			});
+			break
+
+		case "on":
+			//tuya.resolveIds().then(() => {  
+			tuya.get({ 'dps': dps }).then(status => {
+				console.log('Status: ' + status);
+
+				tuya.set({ set: true, 'dps': dps }).then(result => {
+					console.log('Result of setting status to ' + true + ': ' + result);
+					response.setHeader("cmd-response", !status);
+					response.setHeader("onoff", "on");
+					console.log("Status (" + status + ") sent to SmartThings.");
+					response.end();
+
+
+					return;
+
+				});
+				//});
+			});
+			break
+
+		case "status":
+			//tuya.resolveIds().then(() => {  
+			tuya.get({ 'dps': dps }).then(status => {
+				console.log('Status: ' + status);
+
+
+				response.setHeader("cmd-response", status);
+				response.setHeader("onoff", "on");
+				console.log("Status (" + status + ") sent to SmartThings.");
+				response.end();
+
+
+				return;
+
+
+				//});
+			});
+			break
+
+		default:
+			tuya.destroy();
+			console.log('Unknown request');
+
+	}
 }
 
 //----- Utility - Response Logging Function ------------------------
